ab999abd547f17df6ab608c07920154a7aeaf9d5
Manikanta-9391 <manikantaakkannagari@gmail.com>
2026-03-06 09:45:54 +0530

    Manikanta Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Book My Stay - Hotel Booking Management System";
+        String version = "Version 1.0";
+
+        System.out.println("=======================================");
+        System.out.println("        Welcome to " + appName);
+        System.out.println("              " + version);
+        System.out.println("=======================================");
+        System.out.println("Thank you for using our application!");
+        System.out.println("Initializing system...");
+    }
+}
